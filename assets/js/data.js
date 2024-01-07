236af6aff9badd2427c35a680f0e4a0d2cc87bdf
Vlad <vladyn19981998@gmail.com>
2024-01-06 18:34:09 -0600

    feat: implement the music-app logic

---
+++ assets/js/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    link: "LeySeca.mp3",
+    genre: "Reggaeton",
+    track: "Ley Seca",
+    group: "Jhay Cortez ft Anuel AA ",
+    year: 2021,
+  },
+  {
+    id: 2,
+    link: "LoQuePasó,Pasó.mp3",
+    genre: "bachata",
+    track: "Lo Que Pasó, Pasó",
+    group: "Daddy Yankee",
+    year: 2004,
+  },
+  {
+    id: 3,
+    link: "DóndeEstásCorazón.mp3",
+    genre: "Pop",
+    track: "Dónde Estás Corazón",
+    group: "Shakira",
+    year: 1995,
+  },
+  {
+    id: 4,
+    link: "Columbia.mp3",
+    genre: "Reggaeton",
+    track: "Columbia",
+    group: "Quevedo",
+    year: 2023,
+  }
+];
